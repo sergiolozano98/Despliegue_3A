94f60119d61d113e8478b60d407487c16970a71d
2daw <2daw@WINMAC17.alumnado.flo>
2017-12-11 08:37:53 +0000

    subida act 3B

--- provision.sh
+++ provision.sh
@@ -1,3 +1,13 @@
 sudo apt-get -y update
 
 sudo apt-get install -y apache2
+
+cp -r /var/www/html /vagrant/html
+
+rm -r /var/www/html/
+
+ln -fs  /vagrant/html /var/www/html
+
+apt-get install git
+
+git clone https://github.com/sergiolozano98/webopcional.git
